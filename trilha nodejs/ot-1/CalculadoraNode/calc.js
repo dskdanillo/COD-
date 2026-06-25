0d872f325b7ed3470a1236f36b7b4854790ce9bb
Danillo de Souza Koch <dsk.danillo@gmail.com>
2026-06-25 19:17:03 -0300

    ALTERAÇÃO

--- trilha nodejs/ot-1/CalculadoraNode/calc.js
+++ trilha nodejs/ot-1/CalculadoraNode/calc.js
@@ -5,7 +5,7 @@ const MultiplicacaoFunc = require('./multiplicacao');
 const DivisaoFunc     = require('./divisao');
 
 // Executa cada operação e exibe o resultado com template strings
-console.log(`O valor da soma é: ${SomaFunc(1, 2)}`);           // 3
+console.log(`O valor da soma é: ${SomaFunc(1, 2)}`);           //  3
 console.log(`O valor da divisão é: ${DivisaoFunc(10, 2)}`);    // 5
 console.log(`O valor da multiplicação é: ${MultiplicacaoFunc(4, 5)}`); // 20
 console.log(`O valor da subtração é: ${SubtracaoFunc(10, 2)}`); // 8
